d58433234bdeb51945e4dd1bad4e7dc7d166aa36
Martin d'Allens <caerbazar@gmail.com>
2013-05-19 20:49:19 +0200

    Arduino passe pas le DNS

--- arduinoClient/arduinoClient.ino
+++ arduinoClient/arduinoClient.ino
@@ -1,83 +1,91 @@
 /*
-  Web client
- 
- This sketch connects to a website (http://www.google.com)
- using an Arduino Wiznet Ethernet shield. 
- 
+  Baguette Client
+  
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
  
  created 18 Dec 2009
  modified 9 Apr 2012
  by David A. Mellis
+ modified 19 May 2013
+ by Martin d'Allens and Maxime Morin
  
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress google(173,194,45,69); // Google
-IPAddress server(192,168,0,40);
-IPAddress ip(192,168,0,177);
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
+#define SERVER       "dallens.fr"
+#define PORT         1234
+#define STATUS_PIN   13
+
+#define DEBUG(str)   Serial.println(str);
+
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0xA0, 0x28 };
+
+IPAddress dev(192,168,0,40); // Unused.
+IPAddress fixed(192,168,0,199); // Unused.
+
 EthernetClient client;
 
 void setup() {
- // Open serial communications and wait for port to open:
+  pinMode(STATUS_PIN, OUTPUT);
+  
   Serial.begin(9600);
-   while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-
-  // start the Ethernet connection:
-  Ethernet.begin(mac);
-  if (1 == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for(;;)
-      ;
-  }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect("www.google.com", 80)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.0");
-    client.println();
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
+  
+  DEBUG("REBOOT")
 }
+
 
 void loop()
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-
-    // do nothing forevermore:
-    for(;;)
-      ;
-  }
+   int i;
+   
+   DEBUG("LOOP")
+   delay(1000); // Limit the rate of connexions.
+   
+   if (Ethernet.begin(mac) == 0) { // Connectivity or DHCP error.
+      return;
+   }
+   DEBUG("DHCP_OK")
+   
+   if (!client.connect(SERVER, PORT)) {
+      return; // Might be a firewall error.
+   }
+   DEBUG("CONNECT_OK")
+   
+   if (client.available()) {
+      DEBUG("SERVER_FIRST")
+      return; // The server shouldn't send anything first.
+   }
+  
+   client.print("{\"identifier\": \"");
+   for (i = 0; i < 6; i++) {
+      client.print(mac[i], HEX);
+   }
+   client.print("\", \"key\": \"ABCDEF0123456789\"}\r\n");
+   client.flush();
+   
+   DEBUG("ALL_OK")
+   
+   digitalWrite(STATUS_PIN, HIGH);
+   while (client.connected()) {
+      if (client.available()) {
+         Serial.write(client.read());
+      }
+      if (Serial.available()) {
+         client.write(Serial.read());
+      }
+   }
+   digitalWrite(STATUS_PIN, LOW);
+   
+   DEBUG("DISCONNECT")
+   
+   client.stop();
 }
 
